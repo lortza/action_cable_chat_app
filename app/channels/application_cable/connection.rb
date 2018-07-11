e16b1f646b3e29dd14665b0414f69314b5be9b70
anne <richardson.ae@gmail.com>
2018-07-11 14:48:01 -0500

    Adds user authentication security to the connection

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,23 @@
 module ApplicationCable
+  # Built when a new connection is made
   class Connection < ActionCable::Connection::Base
+    include SessionsHelper
+    # Identify the user
+    identified_by :message_user
+
+    def connect
+      # Requires the current_user before connecting
+      self.message_user = find_verified_user
+    end
+
+    private
+
+    def find_verified_user
+      if logged_in?
+        current_user
+      else
+        reject_unauthorized_connection
+      end
+    end
   end
 end
